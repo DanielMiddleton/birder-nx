d3ec41431951b39560bb39ffea116e71d0bd100f
Daniel Middleton <middleton.daniel@gmail.com>
2023-10-02 16:27:44 -0500

    fixup! fixup! Add auto format on pre commit

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,10 +1,7 @@
 // https://www.thisdot.co/blog/linting-formatting-and-type-checking-commits-in-an-nx-monorepo-with-husky/#configuring-lint-staged
 
 module.exports = {
-  '{apps,libs,tools}/**/*.{ts,tsx}': (files) => {
-    return `nx affected --target=typecheck --files=${files.join(',')}`;
-  },
-  '{apps,libs,tools}/**/*.{js,ts,jsx,tsx,json}': [
+  '**/*.{js,ts,jsx,tsx,json}': [
     (files) => `nx affected:lint --files=${files.join(',')}`,
     (files) => `nx format:write --files=${files.join(',')}`,
   ],
