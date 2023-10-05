a4e7c016a77b28b2f05def8c56e66b02fa82d46d
Daniel Middleton <middleton.daniel@gmail.com>
2023-10-05 17:51:28 -0500

    Introduce Azure function to handle file upload (#3)

--- apps/birder-ui/src/app/app.tsx
+++ apps/birder-ui/src/app/app.tsx
@@ -1,12 +1,24 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
-
-import NxWelcome from './nx-welcome';
-
 export function App() {
   return (
     <div>
-      <NxWelcome title="birder-ui" />
+      <form
+        title="Select bird image"
+        encType="multipart/form-data"
+        onSubmit={(e) => {
+          e.preventDefault();
+
+          const { currentTarget: form } = e;
+
+          fetch('http://localhost:7071/api/upload-image', {
+            method: 'POST',
+            body: new FormData(form),
+          });
+        }}
+      >
+        <label htmlFor="image">Bird Image</label>
+        <input type="file" name="image" id="image" />
+        <button type="submit">Upload</button>
+      </form>
     </div>
   );
 }
